56ab613bd2b470f9f2307c415d457b4dfe9d5ddc
Feyza Seyfi <demlikposet@gmail.com>
2025-11-18 16:27:02 +0300

    Create layout for app

---
+++ app/(app)/app/layout.tsx
@@ -0,0 +1,29 @@
+import AppFooter from "@/components/AppFooter";
+import AppHeader from "@/components/AppHeader";
+import BackgroundPattern from "@/components/BackgroundPattern";
+import PetContextProvider from "@/contexts/PetContextProvider";
+import { Pet } from "@/lib/types";
+
+export default async function Layout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const response = await fetch(
+    "https://bytegrad.com/course-assets/projects/petsoft/api/pets"
+  );
+  if (!response.ok) {
+    throw new Error("could not fetch pets");
+  }
+  const data: Pet[] = await response.json();
+  return (
+    <>
+      <BackgroundPattern />
+      <div className="flex flex-col max-w-5xl mx-auto px-4 min-h-screen">
+        <AppHeader />
+        <PetContextProvider data={data}>{children}</PetContextProvider>
+        <AppFooter />
+      </div>
+    </>
+  );
+}
---
+++ components/BackgroundPattern.tsx
@@ -0,0 +1,3 @@
+export default function BackgroundPattern() {
+  return <div className="bg-green-500 h-[300px] w-full absolute top-0 -z-10" />;
+}
