f35b2108da253011f09d9ed88aac012ccd59ac0b
Feyza Seyfi <demlikposet@gmail.com>
2025-11-19 22:59:06 +0300

    Create Account page

--- app/(app)/app/account/page.tsx
+++ app/(app)/app/account/page.tsx
@@ -1,3 +1,13 @@
+import ContentBlock from "@/components/ContentBlock";
+import Heading from "@/components/Heading";
+
 export default function Account() {
-  return <div>Account page</div>;
+  return (
+    <main>
+      <Heading className="text-white my-8 ">Your account</Heading>
+      <ContentBlock className="h-[500px] flex justify-center items-center">
+        <p>Logged in as...</p>
+      </ContentBlock>
+    </main>
+  );
 }
