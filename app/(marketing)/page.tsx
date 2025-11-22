2d6ec2464a8254ddf318da068b40443def045829
Feyza Seyfi <demlikposet@gmail.com>
2025-11-22 21:12:14 +0300

    Add marketing image

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,4 +1,4 @@
-// import Image from "next/image";
+import Image from "next/image";
 
 import Logo from "@/components/logo";
 import { Button } from "@/components/ui/button";
@@ -6,17 +6,21 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="bg-green-500 min-h-screen flex flex-col xl:flex-row items-center justify-center gap-10">
-      {/* insert image here */}
-      {/* <Image src="" width="" height="" alt="" /> */}
-      {/*end of image    */}
-      <div>
+    <main className="bg-green-400  px-8 py-8 min-h-screen flex flex-col-reverse xl:flex-row items-center justify-center gap-10">
+      <Image
+        src="/main.png"
+        width={520}
+        height={410}
+        alt="PetHotel dashboard site view"
+      />
+
+      <div className="">
         <Logo />
-        <h1 className="text-5xl font-semibold my-6 max-w-[500px]">
+        <h1 className="text-4xl md:text-5xl font-semibold my-6 max-w-[500px]">
           Manage your <span className="font-extrabold">pet daycare</span> with
           ease
         </h1>
-        <p className="text-2xl font-medium max-w-[600px]">
+        <p className="text-xl md:text-2xl font-medium max-w-[600px]">
           Use PetSoft to easily keep track of pets under your care, Get lifetime
           access for $299.
         </p>
