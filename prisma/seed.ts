41a73663372d31173fa92148efd534dc1019db03
Feyza Seyfi <demlikposet@gmail.com>
2025-12-15 13:43:09 +0300

    Create user model and re-seed the db

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,44 +1,50 @@
 import { PrismaClient, Prisma } from "../app/generated/prisma/client";
-//import { PrismaPg } from "@prisma/adapter-pg";
-// import "dotenv/config";
+import bcrypt from "bcrypt";
 
-// const adapter = new PrismaPg({
-//   connectionString: process.env.DATABASE_URL,
-// });
+const prisma = new PrismaClient({});
 
-const prisma = new PrismaClient({
-  //adapter,
-});
-
-const petData: Prisma.PetCreateInput[] = [
-  {
-    name: "Benjamin",
-    ownerName: "John Doe",
-    imageUrl: "https://placedog.net/200/300?id=21",
-    age: 2,
-    notes:
-      "Doesn't like to be touched on the belly. Plays well with other dogs.",
+const userData: Prisma.UserCreateInput = {
+  email: "example@gmail.com",
+  hashedPassword: "",
+  pets: {
+    create: [
+      {
+        name: "Benjamin",
+        ownerName: "John Doe",
+        imageUrl: "https://bytegrad.com/course-assets/images/rn-image-4.png",
+        age: 2,
+        notes:
+          "Doesn't like to be touched on the belly. Plays well with other dogs.",
+      },
+      {
+        name: "Richard",
+        ownerName: "Josephine Dane",
+        imageUrl: "https://bytegrad.com/course-assets/images/rn-image-5.png",
+        age: 5,
+        notes: "Needs medication twice a day.",
+      },
+      {
+        name: "Anna",
+        ownerName: "Frank Doe",
+        imageUrl: "https://bytegrad.com/course-assets/images/rn-image-6.png",
+        age: 4,
+        notes: "Allergic to chicken.",
+      },
+    ],
   },
-  {
-    name: "Richard",
-    ownerName: "Josephine Dane",
-    imageUrl: "https://placedog.net/200/300?id=25",
-    age: 5,
-    notes: "Needs medication twice a day.",
-  },
-  {
-    name: "Anna",
-    ownerName: "Frank Doe",
-    imageUrl: "https://placedog.net/200/300?id=54",
-    age: 4,
-    notes: "Allergic to chicken.",
-  },
-];
+};
 
 export async function main() {
-  for (const pet of petData) {
-    await prisma.pet.create({ data: pet });
-  }
+  console.log(`Start seeding ...`);
+
+  const hashedPassword = await bcrypt.hash("example", 10);
+  userData.hashedPassword = hashedPassword;
+
+  await prisma.user.create({
+    data: userData,
+  });
+
+  console.log(`Seeding finished.`);
 }
 
 main();
